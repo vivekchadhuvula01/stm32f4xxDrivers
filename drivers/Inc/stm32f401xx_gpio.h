02d2e231fddd668c3c48948925080bb5b4176e0b
vivekchadhuvula01 <vivekchadhuvula@gmail.com>
2025-04-14 22:30:13 +0530

    GPIO DEFINES are done

--- drivers/Inc/stm32f401xx_gpio.h
+++ drivers/Inc/stm32f401xx_gpio.h
@@ -33,4 +33,62 @@ typedef struct
 } GPIO_Handle_t;
 
 
+
+/*
+ * @GPIO_PIN_NUMBERS
+ * GPIO pin numbers
+ */
+
+#define GPIO_PIN_0    0
+#define GPIO_PIN_1    1
+#define GPIO_PIN_2    2
+#define GPIO_PIN_3    3
+#define GPIO_PIN_4    4
+#define GPIO_PIN_5    5
+#define GPIO_PIN_6    6
+#define GPIO_PIN_7    7
+#define GPIO_PIN_8    8
+#define GPIO_PIN_9    9
+#define GPIO_PIN_10    10
+#define GPIO_PIN_11    11
+#define GPIO_PIN_12    12
+#define GPIO_PIN_13    13
+#define GPIO_PIN_14    14
+#define GPIO_PIN_15    15
+
+
+
+/*
+ * @GPIO_PIN_MODES
+ * GPIO pin possible modes
+ */
+
+#define GPIO_MODE_IN          0
+#define GPIO_MODE_OUT       1
+#define GPIO_MODE_ALTFN     2
+#define GPIO_MODE_ANALOG  3
+#define GPIO_MODE_IT_FT      4
+#define GPIO_MODE_IT_RT     5
+#define GPIO_MODE_IT_RFT    6
+
+/*
+ * @GPIO_PIN_SPEED
+ * GPIO pin possible output speeds
+ */
+#define GPIO_SPEE_LOW  0
+#define GPIO_SPEE_MEDIUM  1
+#define GPIO_SPEE_FAST  2
+#define GPIO_SPEE_HIGH   3
+
+
+/*
+ * GPIO pin pull up AND pull down configuration macros
+ */
+#define GPIO_NO_PUPD   0
+#define GPIO_PIN_PU      1
+#define GPIO_PIN_PD      2
+
+
+
+
 #endif /* INC_STM32F401XX_GPIO_H_ */
